15ff1e3bc2058dd7c872334d36ab16db97d19bd6
Solomon Stevens <funnyquestions4jbq@gmail.com>
2024-05-22 10:47:15 -0400

    Begin producer code

---
+++ temperature_producer.py
@@ -0,0 +1,6 @@
+"""
+- Program to read in temperatures from provided CSV file
+- Will send temperatures to respective consumers based on
+  what we are reading the temperatures of (smoker, foods)
+
+"""
